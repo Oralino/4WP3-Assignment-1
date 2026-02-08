b06e40eeda91314d9b63c3871aa2ed2036d2ed80
Noor Alnajar <nooralnajar1997@outlook.com>
2026-02-08 02:03:45 -0500

    create base input for title and desc and added landmark data holder

--- Assignment 1/app.js
+++ Assignment 1/app.js
@@ -1,11 +1,13 @@
-let currentImageBlob = null; // In-memory storage
+
+// stores images locally
+let currentImageBlob = null; 
 
 document.getElementById('photo-input').addEventListener('change', function(e) {
     const file = e.target.files[0];
     if(file) {
         const reader = new FileReader();
         reader.onload = function(event) {
-            currentImageBlob = event.target.result; // Store Base64 string
+            currentImageBlob = event.target.result; 
             const img = document.getElementById('preview-image');
             img.src = currentImageBlob;
             img.style.display = 'block';
@@ -13,3 +15,16 @@ document.getElementById('photo-input').addEventListener('change', function(e) {
         reader.readAsDataURL(file);
     }
 });
+
+//landmark storage and data object
+let landmarks = [];
+
+function createLandmarkObject(title, desc, lat, lng, image) {
+    return {
+        id: Date.now(),
+        title: title,
+        description: desc,
+        location: { lat: parseFloat(lat), lng: parseFloat(lng) },
+        image: image
+    };
+}
